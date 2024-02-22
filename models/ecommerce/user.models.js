fce9e97704265f6154fc77923bc64267ba1ab743
Pratik-Hulawale <pratikhulawale18@gmail.com>
2024-02-22 06:06:39 +0530

    commit

--- models/ecommerce/user.models.js
+++ models/ecommerce/user.models.js
@@ -1,4 +1,21 @@
 import mongoose from "mongoose"
 const userSchema=new mongoose.sanitizeFilter({
-  
-})
+  userName:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true
+  },
+  password:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true
+  },
+},{timestamps:true})
